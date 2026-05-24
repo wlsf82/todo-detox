022d143135e721570dcb4cc924b3bbe90d3684d8
Walmyr <wlsf82@gmail.com>
2026-05-24 17:50:18 +0200

    Delete tests implementation
    To demo the implementation during the YouTube live-coding.

--- e2e/todo-detox.test.js
+++ e2e/todo-detox.test.js
@@ -1,134 +1,61 @@
 describe('TODO App', () => {
   beforeAll(async () => {
-    await device.launchApp();
+    // TODO
   });
 
   beforeEach(async () => {
-    await device.reloadReactNative();
+    // TODO
   });
 
   describe('Create', () => {
     it('adding a new TODO shows the remaining counter and clears the input field', async () => {
-      await element(by.id('new-todo-input')).typeText('Buy groceries');
-      await element(by.id('add-todo-button')).tap();
-
-      await expect(element(by.text('Buy groceries'))).toBeVisible();
-      await expect(element(by.id('todo-count'))).toHaveText('1 of 1 remaining');
-      await expect(element(by.id('new-todo-input'))).toHaveText('');
+      // TODO
     });
 
     it('adds multiple TODOs', async () => {
-      await element(by.id('new-todo-input')).typeText('First task');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('new-todo-input')).typeText('Second task');
-      await element(by.id('add-todo-button')).tap();
-
-      await expect(element(by.text('First task'))).toBeVisible();
-      await expect(element(by.text('Second task'))).toBeVisible();
+      // TODO
     });
 
     it('does not add a TODO when input is empty', async () => {
-      await element(by.id('add-todo-button')).tap();
-
-      await expect(element(by.id('todo-list'))).not.toExist();
+      // TODO
     });
   });
 
   describe('Read', () => {
     it('shows the empty state when there are no TODOs', async () => {
-      await expect(element(by.text('No TODOs yet.'))).toBeVisible();
+      // TODO
     });
   });
 
   describe('Update', () => {
     it('edits a TODO', async () => {
-      await element(by.id('new-todo-input')).typeText('Original text');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('edit-todo-1')).tap();
-      await waitFor(element(by.id('edit-todo-input')))
-        .toBeVisible()
-      await element(by.id('edit-todo-input')).clearText();
-      await element(by.id('edit-todo-input')).typeText('Updated text');
-      await element(by.id('save-edit-button')).tap();
-
-      await expect(element(by.text('Updated text'))).toBeVisible();
-      await expect(element(by.text('Original text'))).not.toExist();
+      // TODO
     });
 
     it('dismisses the edit modal on cancel', async () => {
-      await element(by.id('new-todo-input')).typeText('Some task');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('edit-todo-1')).tap();
-      await waitFor(element(by.id('edit-modal')))
-        .toBeVisible()
-
-      await element(by.id('cancel-edit-button')).tap();
-      await expect(element(by.id('edit-modal'))).not.toBeVisible();
+      // TODO
     });
 
     it('marks a TODO as completed', async () => {
-      await element(by.id('new-todo-input')).typeText('Complete me');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('toggle-todo-1')).tap();
-
-      await waitFor(element(by.id('todo-count')))
-        .toHaveText('0 of 1 remaining')
+      // TODO
     });
 
     it('updates the counter when a TODO is completed', async () => {
-      await element(by.id('new-todo-input')).typeText('Task A');
-      await element(by.id('add-todo-button')).tap();
-      await element(by.id('new-todo-input')).typeText('Task B');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('toggle-todo-1')).tap();
-
-      await waitFor(element(by.id('todo-count')))
-        .toHaveText('1 of 2 remaining')
+      // TODO
     });
 
     it('unmarks a completed TODO', async () => {
-      await element(by.id('new-todo-input')).typeText('Toggle me');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('toggle-todo-1')).tap();
-      await waitFor(element(by.id('todo-count')))
-        .toHaveText('0 of 1 remaining')
-
-      await element(by.id('toggle-todo-1')).tap();
-      await waitFor(element(by.id('todo-count')))
-        .toHaveText('1 of 1 remaining')
+      // TODO
     });
   });
 
   describe('Delete', () => {
     it('deletes a TODO and shows the empty state', async () => {
-      await element(by.id('new-todo-input')).typeText('Delete me');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('delete-todo-1')).tap();
-      await waitFor(element(by.id('delete-confirm-modal')))
-        .toBeVisible()
-      await element(by.id('confirm-delete-button')).tap();
-
-      await expect(element(by.text('Delete me'))).not.toExist();
-      await expect(element(by.text('No TODOs yet.'))).toBeVisible();
+      // TODO
     });
 
     it('cancels deletion when tapping Cancel on the confirmation', async () => {
-      await element(by.id('new-todo-input')).typeText('Keep me');
-      await element(by.id('add-todo-button')).tap();
-
-      await element(by.id('delete-todo-1')).tap();
-      await waitFor(element(by.id('delete-confirm-modal')))
-        .toBeVisible()
-      await element(by.id('cancel-delete-button')).tap();
-
-      await expect(element(by.text('Keep me'))).toBeVisible();
+      // TODO
     });
   });
 });
